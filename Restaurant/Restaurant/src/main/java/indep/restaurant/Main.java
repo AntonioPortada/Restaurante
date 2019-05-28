87065fdfe69ea347ba6fb79c13c98f79106d2ed2
Administrador <Administrador@usuario-PC>
2019-05-28 18:14:04 -0500

    ventanas

---
+++ Restaurant/Restaurant/src/main/java/indep/restaurant/Main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package indep.restaurant;
+
+import indep.presentacion.UILogin;
+
+/**
+ *
+ * @author Administrador
+ */
+public class Main {
+    
+    public static void main(String[] args){
+        new UILogin().setVisible(true);
+    }
+}
